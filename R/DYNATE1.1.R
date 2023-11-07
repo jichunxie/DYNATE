44f1fec4a386661a38ffdeeaabb01276335b73b4
JintongZhao <127447810+JintongZhao@users.noreply.github.com>
2023-11-06 23:33:57 -0500

    Update and rename DYNATE.R to DYNATE1.1.R

--- R/DYNATE1.1.R
+++ R/DYNATE1.1.R
@@ -31,19 +31,24 @@
 #' p.leaf <- Test_Leaf(snp_dat=snp_dat,thresh_val=M,covars=c("X1","X2"),teststat="score")
 #' summary(p.leaf)
 #'
-Test_Leaf <- function(snp_dat=NULL,thresh_val=10,covars=NULL,teststat="FET"){
+Test_Leaf1 <- function(snp_info=NULL,sample_info = NULL, thresh_val=10,covars=NULL,teststat="FET"){
+  sample_info <- sample_info %>% select("Sample.Name","Sample.Type") %>% unique()
+  snp_info <- snp_info %>% select("Sample.Name","snpID","domainID")
+  
+  # Prove we can gain former input data by using new input data
+  snp_dat <- left_join(snp_info, sample_info, by = "Sample.Name")
   struct_map <- construct_leafs(snp_dat=snp_dat,thresh_val=thresh_val)
   total_leaves <- uniqueN(struct_map$L1)
   #Global objects
   D_approx_prev = FD_approx_prev = 0
-
+  
   glm_input <- snp_dat%>%
     dplyr::select(c("Sample.Name","Sample.Type",covars))%>%
     distinct_all()%>%
     arrange(Sample.Name)%>%
     mutate(types=ifelse(Sample.Type=="case",1,0))%>%
     dplyr::select(c("types",covars))
-
+  
   N <- uniqueN(snp_dat$Sample.Name)
   nsnp <- max(as.integer(snp_dat$snpID))
   # Get Gmat case and control
@@ -53,21 +58,21 @@ Test_Leaf <- function(snp_dat=NULL,thresh_val=10,covars=NULL,teststat="FET"){
                  Dim = c(N, nsnp))
   N1=sum(glm_input$types)
   N0=N-N1
-
+  
   leaf_mat_all <- create_leaf_attribute(mat_all,struct_map)
   leaf_mat_all@x <- ifelse(leaf_mat_all@x>0,1,0)
-
+  
   if(teststat=="FET"){
     #Get marginals - qualifying variants for cases and all
     case_colSums = Matrix::colSums(leaf_mat_all[1:N1,])
     all_colSums = Matrix::colSums(leaf_mat_all)
-
+    
     pvals.1 <- calcFETpval_per_leaf(N1=N1,N0=N0,
                                     case_colSums = case_colSums,
                                     all_colSums = all_colSums,
                                     midp=TRUE)
     pvals.1 <- pmin(pvals.1,1)
-
+    
   } else{
     if(ncol(glm_input)>1){
       #With covariates
@@ -83,7 +88,7 @@ Test_Leaf <- function(snp_dat=NULL,thresh_val=10,covars=NULL,teststat="FET"){
     score.1 <- score.test$Tstat.sign
     pvals.1 <- score.test$p.value
   }
-
+  
   Z.1 <- qnorm(1-pvals.1)
   pvals.1[is.na(pvals.1)] <- 1
   Z.1[is.na(pvals.1)] <- -100
@@ -95,7 +100,7 @@ Test_Leaf <- function(snp_dat=NULL,thresh_val=10,covars=NULL,teststat="FET"){
     left_join(struct_map,by="L1")%>%filter(pvals!=1)%>%#mutate(L0=L1)%>%
     #mutate(L0=rleid(L1))%>%
     mutate("Test"=teststat)
-
+  
   return(struct_map0)
 }
 
@@ -130,35 +135,41 @@ Test_Leaf <- function(snp_dat=NULL,thresh_val=10,covars=NULL,teststat="FET"){
 #' # conduct dynamic and hierarchical testing based on the leaf level p values.
 #' out <- DYNATE(struct_map=p_leaf,L=L,alpha=alpha)
 #' summary(out)
-DYNATE <- function(struct_map,
-                  L=5,
-                  alpha=0.05){
-
+DYNATE6 <- function(struct_map,
+                    L=5,
+                    alpha=0.05){
+  
   total_leaves <- uniqueN(struct_map$L1)
-
+  
   struct_map <- data.table(struct_map)%>%mutate(L0=L1,L1=rleid(L1))
   pvals.1 <- struct_map[,c("pvals","L1")]%>%distinct_all()
   pvals.1 <- c(struct_map$pvals)
-
+  
   # get the estimated mixed node structure
   total_leaves <- uniqueN(struct_map$L1)
   hatn1 <- ceiling(sqrt(total_leaves))
   pn1 <- pvals.1[rank(pvals.1,ties.method="first")==hatn1]
-
+  
   setkey(struct_map,snpID)
   struct_map <- struct_map %>% mutate(hatm1=(pvals<=pn1),pvals1=pvals) %>%
     group_by(L1)%>%mutate(wt=1/n())%>%ungroup()
-
+  
   struct_map_ <- struct_map
-
+  
   Sps <- NULL
   p1s <- p0s <- NULL
   smap_res <- NULL
   S<-NULL
   D_approx_prev = FD_approx_prev = 0
+  
+  #pvals_list <- list() #add
+  
+  #Ll <- paste0("L",1) # 添加这一行，定义初始值为 L1
+  
   for(l in seq(L)){
     Ll <- paste0("L",l)
     Lm1 <- paste0("L",l-1)
+    
     if(l>1){
       #First remove domains with fewer than 2^(l-1) L1 leaves
       removed_map <- struct_map%>%
@@ -167,7 +178,7 @@ DYNATE <- function(struct_map,
       struct_map <- struct_map%>%
         group_by(domainID)%>%filter_at(Lm1,any_vars(uniqueN(.)>=2))%>%
         ungroup()
-
+      
       setDT(struct_map)
       setkey(struct_map, domainID)
       struct_map <- struct_map[, (Ll) := {
@@ -179,22 +190,43 @@ DYNATE <- function(struct_map,
         }
         x
       }, by = .(domainID)][, (Ll) := rleid(domainID,get(Ll))] #reassign groups by domain and Ll
-
+      
       struct_map <- struct_map%>%group_by_at(Ll)%>%#mutate(wt=)%>%
         mutate(pvals=stouffer.p2(pvals1,L1,wt))%>%ungroup()
       Sps <- samp.pvals.leaf_L1(struct_map,Ll,p1s=p1s,p0s=p0s)
+      
+      struct_map <- struct_map %>% mutate(!!paste0("pvals-", l) := pvals) #add
     }
-
+    
+    
+    # Ll <- paste0("L",l) # 添加这一行，将 Ll 更新为当前循环的 L 值
+    
     m.l = uniqueN(data.frame(struct_map)[,Ll])
+    
+    
+    #pvals_cols <- grep("^pvals-", colnames(struct_map)) # add
+    #struct_map_pvals <- struct_map[, pvals_cols] # add
+    #  pvals.l = data.frame(struct_map_pvals[,l-1]) %>% unique() # add
+    #pvals.l = data.frame(struct_map_pvals[,l-1]) # add
+    #pvals.l = pvals.l[!duplicated(pvals.l), ] # add
+    
+    #pvals.l = unlist(pvals.l) # add
+    
     pvals.l=struct_map%>%dplyr::select(c("pvals",all_of(Ll)))%>%distinct_all()
+    
+    
+    
     #Obtain layer specific-threshold
     p.hat = est.p.hat_samp(l=l,
                            D_prev = D_approx_prev,
                            FD_prev = FD_approx_prev,
+                           #pvals_l =pvals.l,# add
                            pvals_l=c(pvals.l$pvals),
                            alpha=alpha,
                            alpha1=NULL,
                            Sps=Sps)
+    
+    
     p.hat.l=p.hat$p.hat
     D_approx_prev = p.hat$D_approx_prev
     FD_approx_prev = p.hat$FD_approx_prev
@@ -207,7 +239,7 @@ DYNATE <- function(struct_map,
       }else{
         p1s=pvals.1[S.l1]
         p0s=pvals.1[-S.l1]
-
+        
         #new (R(1-alpha)), more conservative, 02242022
         p1s1=sort(p1s)[seq(floor(length(p1s)*(1-alpha)))]
         p1s2=sort(p1s)[-seq(floor(length(p1s)*(1-alpha)))]
@@ -222,8 +254,8 @@ DYNATE <- function(struct_map,
     if(nrow(struct_map)==0){next}
   }
   smap_res <- smap_res%>%mutate(L1=L0)%>%
-    dplyr::select(-c("pvals","hatm1","wt","L0","L2","L3"))
-
+    dplyr::select(-c("pvals","hatm1","wt","L0"))
+  
   return(smap_res)
 }
 
@@ -256,42 +288,42 @@ construct_leafs <- function(snp_dat,
     dplyr::select(-c(contains("group"),lastObsFlagDomain,num_unique,num_comb,num_all)) %>%
     mutate_at("Sample.Name",as.factor) %>% #coerce ID back to factor
     data.table() #15-37s, based on N0 size
-
+  
   struct_map <- struct_map %>% dplyr::select(c(snpID,L1,domainID)) %>% distinct(snpID,L1,domainID)
   setkey(struct_map,snpID)
   t2 <- Sys.time()
-
+  
   return(struct_map)
 }
 
 
 est.p.hat_samp <- function(l,D_prev,FD_prev,pvals_l,alpha,alpha1=NULL,Sps=NULL){
-
+  
   ##print(paste("l:",l))
   if(is.null(alpha1)) {alpha1=alpha}
   m.l = length(pvals_l)
-
+  
   #Threshold very small p-values based on constant
   p.m = ifelse(m.l==0,1,min(1/(m.l*sqrt(log(m.l))),0.05))
-
+  
   filter <- which(pvals_l<=p.m|pvals_l>alpha1)
-
+  
   if(length(filter)>0){
     p.vec = unique(sort(pvals_l[-filter],decreasing = FALSE))
   }else {
     p.vec = unique(sort(pvals_l,decreasing = FALSE))
   }
   if(length(p.vec)==0){p.vec=p.m}
-
+  
   p.indx = 0
   emp.fdr = 0
-
+  
   addi <- 0
   if(!is.null(Sps)){
     propct <- Sps$sps$ct*Sps$sps$altprop
     delnames <- intersect(c("type","altct","nullct","altprop","ct"),colnames(Sps$sps))
     sps <- Sps$sps%>%dplyr::select(-delnames)
-
+    
     rows=nrow(sps);cols=ncol(sps)
     if(rows==1){
       sps0 <- sapply(sps,function(x){propct%*%outer(x,p.vec,"<=")})
@@ -303,10 +335,10 @@ est.p.hat_samp <- function(l,D_prev,FD_prev,pvals_l,alpha,alpha1=NULL,Sps=NULL){
       }else{
         addi <- rowMeans(sps0)
       }
-
+      
     }
   }
-
+  
   fdr.num = FD_prev + m.l*p.vec+addi
   fdr.denom = D_prev + sapply(p.vec,function(x){sum(pvals_l<=x)})
   emp.fdr = fdr.num/pmax(fdr.denom,1)
@@ -317,7 +349,7 @@ est.p.hat_samp <- function(l,D_prev,FD_prev,pvals_l,alpha,alpha1=NULL,Sps=NULL){
   p.hat1 <- p.vec[index1]
   D_approx_prev=fdr.denom[index]
   FD_approx_prev=min(fdr.num[index],D_approx_prev)
-
+  
   return(list("p.hat"=p.hat,"p.hat1"=p.hat1,
               "FD_approx_prev"=FD_approx_prev,
               "D_approx_prev"=D_approx_prev))
@@ -328,18 +360,18 @@ est.p.hat_samp <- function(l,D_prev,FD_prev,pvals_l,alpha,alpha1=NULL,Sps=NULL){
 ####### Functions to compute test-statistics
 
 fisher.exact.test <- function(z,midp=TRUE){
-
+  
   x <- z[1]
   sampTot <- z[2]
   pop1Tot <- z[3]
   pop2Tot <- z[4]
-
+  
   lo <- max(0L, sampTot - pop2Tot)
   hi <- min(sampTot, pop1Tot)
-
+  
   support <- lo : hi
   out <- dhyper(support, pop1Tot, pop2Tot, sampTot)
-
+  
   if(midp){
     #mid p-val with minimum likelihood method
     return(sum(out[out < out[x - lo + 1]]) + sum(out[out==out[x-lo+1]])/2)
@@ -351,17 +383,17 @@ fisher.exact.test <- function(z,midp=TRUE){
 
 calcFETpval_per_leaf <- function(N1,N0,case_colSums,all_colSums,
                                  midp=TRUE){
-
+  
   #Order of input is: sample hit, sample size, pop1 size, pop2 size
   cont_tab_summ2x2 <- unname(cbind(case_colSums,all_colSums,N1,N0))
-
+  
   # Apply row-wise
   FET_pvals <- apply(cont_tab_summ2x2, 1,
                      function(z) fisher.exact.test(z,midp = midp))
-
+  
   #Vector of length m.l, where m.l is number of leaves/hypotheses at layer l
   return(FET_pvals)
-
+  
 }
 
 compute_counts <- function(thresh, ID, domain_end,snp_end) {
@@ -374,7 +406,7 @@ compute_counts <- function(thresh, ID, domain_end,snp_end) {
       count <<- count + 1L
     }
     countall <<- countall+ 1L
-
+    
     if ((snp_end & (uniqueN(seen_ids) >= thresh))|domain_end) {
       count <- count # copy enclosed value locally
       countall <- countall
@@ -389,16 +421,16 @@ compute_counts <- function(thresh, ID, domain_end,snp_end) {
 
 
 create_leaf_attribute <- function(snp_mat,snp_leaf_map){
-
+  
   snp_mat@Dimnames <- list(NULL,NULL)
-
+  
   #Convert mat to dgTMatrix if not already
   if(!inherits(snp_mat,"dgTMatrix")){
     snp_mat <- as(snp_mat, "dgTMatrix")
   }
-
+  
   snp_mat2 <- snp_mat #copy object
-
+  
   #Replace column indices with new set of indices
   #Make sure initial indices start with zero
   snp_mat2@j <- as.integer(snp_leaf_map[.(snp_mat@j+1)]$L1-1)
@@ -408,7 +440,7 @@ create_leaf_attribute <- function(snp_mat,snp_leaf_map){
   snp_mat2@j <- smij[,2]
   snp_mat2@Dim <- as.integer(c(nrow(snp_mat2),
                                length(unique(snp_leaf_map$L1))))
-
+  
   #Convert to dgCMatrix
   y <- as(snp_mat2,"dgCMatrix")
   return(y)
